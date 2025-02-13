060d7a4ea6f49c7590150e6ff945b9bb86bdea84
nosleep <123565049+MRTNKuchar@users.noreply.github.com>
2025-02-13 02:35:15 +0100

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,5 @@
+#V úloze twosum se snažíme najít z číselné sady (která mají indexy od 0 až po daný počet čísel) součet který máme zadaný
+#např pro hodnoty v mém řešení máme pro čísla v sadě 1, 2, 5 se snažíme najít čísla která se rovnají sedmi
 class Solution: 
     def twoSum(seelf, nums: List[int], target: int) -> List[int]:
         num_dict = {}
